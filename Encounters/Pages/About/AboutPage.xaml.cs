6ca14d397829a5357479f52291370bc7544e64e8
manzanit0 <unico.javier@gmail.com>
2020-04-05 20:33:59 +0100

    Add About page

---
+++ Encounters/Pages/About/AboutPage.xaml.cs
@@ -0,0 +1,12 @@
+using Xamarin.Forms;
+
+namespace Encounters.Pages.About
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Encounters/Pages/MainMenu.xaml.cs
+++ Encounters/Pages/MainMenu.xaml.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using Encounters.Models;
+using Encounters.Pages.About;
 using Encounters.Pages.Liturgy;
 using Encounters.Pages.Locations;
 using Encounters.Pages.Thoughts;
@@ -13,51 +15,28 @@ namespace Encounters.Pages
 
         public MainMenu()
         {
-            // Set the binding context to this code behind.
             BindingContext = this;
 
-            // Build the Menu
             MainMenuItems = new List<MainMenuItem>()
             {
                 new MainMenuItem {Title = "Liturgy", Icon = IconFont.BookCross, TargetType = typeof(LiturgyEntryPage)},
                 new MainMenuItem {Title = "Nearby Churches", Icon = IconFont.MapMarkerRadius, TargetType = typeof(NearbyChurchesPage)},
                 new MainMenuItem {Title = "Thoughts", Icon = IconFont.LeadPencil, TargetType = typeof(NotesPage)},
-                new MainMenuItem {Title = "About", Icon = IconFont.InformationOutline, TargetType = typeof(NotesPage)},
+                new MainMenuItem {Title = "About", Icon = IconFont.InformationOutline, TargetType = typeof(AboutPage)},
             };
 
-            // Set the default page, this is the "home" page.
             Detail = new NavigationPage(new NearbyChurchesPage());
 
             InitializeComponent();
         }
 
-        // When a MenuItem is selected.
         public void MainMenuItem_Selected(object sender, SelectedItemChangedEventArgs e)
         {
-            if (e.SelectedItem is MainMenuItem item)
-            {
-                if (item.Title.Equals("Liturgy"))
-                {
-                    Detail = new NavigationPage(new LiturgyEntryPage());
-                }
-
-                else if (item.Title.Equals("Nearby Churches"))
-                {
-                    Detail = new NavigationPage(new NearbyChurchesPage());
-                }
-                else if (item.Title.Equals("Thoughts"))
-                {
-                    Detail = new NavigationPage(new NotesPage());
-                }
-
-                else if (item.Title.Equals("About"))
-                {
-                    Detail = new NavigationPage(new NotesPage());
-                }
-
-                MenuListView.SelectedItem = null;
-                IsPresented = false;
-            }
+            if (!(e.SelectedItem is MainMenuItem item)) return;
+            
+            Detail = new NavigationPage((Page) Activator.CreateInstance(item.TargetType));
+            MenuListView.SelectedItem = null;
+            IsPresented = false;
         }
     }
 }
